8617378b814b28ecae34ef89d82bf65b7d4d2740
6EmptyBucket9 <jarno.ewijk@gmail.com>
2024-05-23 18:43:21 +0200

    getById added

--- java-spring-vacaturebank-backend/src/main/java/org/acme/javaspringvacaturebankbackend/controller/vacancyController.java
+++ java-spring-vacaturebank-backend/src/main/java/org/acme/javaspringvacaturebankbackend/controller/vacancyController.java
@@ -1,6 +1,7 @@
 package org.acme.javaspringvacaturebankbackend.controller;
 
 import java.util.Map;
+import java.util.Optional;
 
 import org.acme.javaspringvacaturebankbackend.model.vacancyModel;
 import org.acme.javaspringvacaturebankbackend.service.vacancyService;
@@ -14,38 +15,41 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
 @RequestMapping(value = "/api/v1/vacancies")
 public class vacancyController {
     @Autowired
     private vacancyService vacancyService;
 
-    // GET Method to get all vacancie
+    // GET Method to get all vacancies
     @GetMapping
     public Iterable<vacancyModel> getAllVacancies() {
         return vacancyService.getVacancies();
     }
-    
+
+    // GET Method to get vacancy by id
+    @GetMapping(value = "/{id}")
+    public Optional<vacancyModel> getVacancyById(@PathVariable int id) {
+        return vacancyService.getVacancyById(id);
+    }
+
     // POST method to post vacancies
     @PostMapping
     public vacancyModel postVacancy(@RequestBody vacancyModel vacancy) {
-      return vacancyService.postVacancy(vacancy);
+        return vacancyService.postVacancy(vacancy);
     }
 
     // Patch method for updating vacancies
     @PatchMapping(value = "/patch/vacancy/{id}")
-    public vacancyModel vacancyModel (@PathVariable(value = "id", required = false) int id, @RequestBody Map<String, Object> fields)  {
+    public vacancyModel vacancyModel(@PathVariable(value = "id", required = false) int id,
+            @RequestBody Map<String, Object> fields) {
         return vacancyService.patchVacancy(id, fields);
     }
-    
+
     // Delete method for deleting vacancies
     @DeleteMapping(value = "/delete/vacancy/{id}")
-    public String vacancyModel  (@PathVariable(value = "id", required = false) int id){
+    public String vacancyModel(@PathVariable(value = "id", required = false) int id) {
         return vacancyService.deleteVacancy(id);
-    } 
-
-    
+    }
 
 }
